142c1ee2d7419ab76d3957fc8798c778f872a872
Clément Jonglez <clement@jonglez.space>
2026-01-09 19:23:59 +0100

    replace hardcoded max number of authors by cli argument, format code

--- README.md
+++ README.md
@@ -10,10 +10,11 @@ Run the CLI with `bib2yaml`. CLI options are:
 
 * `-i` or `--input-file`: input file path. Defaut is `pubs.bib`
 * `-o` or `--output-file`: output file path. Defaut is `pubs.yml`
+* `-n` or `--n-max-authors`: max numbers of authors to display. When higher than this treshold, only the first author is shown, the rest is displayed as "et al".
 
 ## Usage as library
 
 ```python
 from bib2yaml.bib2yaml import convert_bib
-convert_bib("<INPUT PATH>", "<OUTPUT PATH>")
+convert_bib("<INPUT PATH>", "<OUTPUT PATH>", n_max_authors)
 ```
--- src/bib2yaml/bib2yaml.py
+++ src/bib2yaml/bib2yaml.py
@@ -1,44 +1,54 @@
 import argparse
-from pathlib import Path
 import bibtexparser
 from pylatexenc.latex2text import LatexNodes2Text
 import yaml
 
-def convert_bib(input_file: str, output_file: str):
+
+def convert_bib(input_file: str, output_file: str, n_max_authors: int):
     lt = LatexNodes2Text()
 
     months_to_num_dict = {
-        "jan": 1, "january": 1,
-        "feb": 2, "february": 2,
-        "mar": 3, "march": 3,
-        "apr": 4, "april": 4,
+        "jan": 1,
+        "january": 1,
+        "feb": 2,
+        "february": 2,
+        "mar": 3,
+        "march": 3,
+        "apr": 4,
+        "april": 4,
         "may": 5,
-        "jun": 6, "june": 6,
-        "jul": 7, "july": 7,
-        "aug": 8, "august": 8,
-        "sep": 9, "sept": 9, "september": 9,
-        "oct": 10, "october": 10,
-        "nov": 11, "november": 11,
-        "dec": 12, "december": 12
+        "jun": 6,
+        "june": 6,
+        "jul": 7,
+        "july": 7,
+        "aug": 8,
+        "august": 8,
+        "sep": 9,
+        "sept": 9,
+        "september": 9,
+        "oct": 10,
+        "october": 10,
+        "nov": 11,
+        "november": 11,
+        "dec": 12,
+        "december": 12,
     }
 
     new_output = []
 
-
     def null_checker(bib_entry, key):
-        return bib_entry[key] if key in bib_entry and bib_entry[key] is not None else None
-
+        return (
+            bib_entry[key] if key in bib_entry and bib_entry[key] is not None else None
+        )
 
     with open(input_file) as f:
         db = bibtexparser.load(f)
 
-
     for entry in db.entries:
-
         # TODO: Check if the any of the visible name is same as the CV author, if yes then encapsulate in "***"
 
         authors = [author.strip() for author in entry["author"].split("and")]
-        if len(authors) > 7:
+        if len(authors) > n_max_authors:
             authors = [authors[0], "et al."]
 
         filtered_entry = {
@@ -59,21 +69,42 @@ def convert_bib(input_file: str, output_file: str):
 
         filtered_entry["date"] = full_date
 
-        new_output.append({key: value for key, value in filtered_entry.items() if value is not None})
+        new_output.append(
+            {key: value for key, value in filtered_entry.items() if value is not None}
+        )
 
     with open(output_file, "w") as yaml_file:
         yaml.dump(new_output, yaml_file, sort_keys=False)
 
     print(f"Filtered entries have been written to {output_file}")
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Tool to convert BibTeX bibliography to a YAML file')
-    parser.add_argument("-i", "--input-file", type=str, help="Input file path", default="pubs.bib")
-    parser.add_argument("-o", "--output-file", type=str, help="Output file path, optional", default="pubs.yml")
+    parser = argparse.ArgumentParser(
+        description="Tool to convert BibTeX bibliography to a YAML file"
+    )
+    parser.add_argument(
+        "-i", "--input-file", type=str, help="Input file path", default="pubs.bib"
+    )
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        type=str,
+        help="Output file path, optional",
+        default="pubs.yml",
+    )
+    parser.add_argument(
+        "-n",
+        "--n-max-authors",
+        type=int,
+        help="Max number of authors to display, When higher than this treshold, only the first author is shown, the rest is displayed as 'et al'",
+        default=7,
+    )
 
     args = parser.parse_args()
 
-    convert_bib(Path(args.input_file), Path(args.output_file))
+    convert_bib(args.input_file, args.output_file, args.n_max_authors)
+
 
 if __name__ == "__main__":
     main()
